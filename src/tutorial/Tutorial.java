49ed6ef503ef6fbad1cd6be46e34730378d89d18
Diego Alvarado <Diego Alvarado>
2017-08-15 22:50:10 -0300

    operaciones matematicas en java

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -16,8 +16,8 @@ package tutorial;
  * int -> numeros enteros
  * long -> numeos enteros largos
  * float -> numeros flotantes -> 2.12, 1.432, etc
- * double -> numeros dobles
- * byte ->  se maneja en bytes
+ * double -> numeros dobles - practicamente todos los numeros
+ * byte ->  se maneja en bytes - 8 bits
  * char -> caracteres
  * boolean -> verdaderos o falsos
  * String -> cadena de texto
@@ -31,7 +31,38 @@ public class Tutorial {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo");
+       int variable1;
+       variable1 = 5;
+       int resultado;
+       resultado = variable1 * 2;
+       System.out.println(variable1);
+       System.out.println("El resultado es : "+resultado);
+       int res2;
+       res2 = variable1 / 2; /*mostrara el numero entero*/
+       System.out.println("El numero entero de la division es: "+res2);
+       int res3;
+       res3 = variable1 % 2; /*mostrara el residuo de la division, en este caso 1*/
+       System.out.println("El residuo de la division es: "+res3);
+       float v1, r1; /*con dato double es lo mismo*/
+       v1 = 5;
+       r1 = v1 / 2;
+       System.out.println("La division completa es: "+r1);
+       
+       /*Ejercicio*/
+       short suma;
+       suma = 3+10;
+       int resta = 8- 17;
+       long residuo = 9/2;
+       float multiplicacion = 2*(15*(-2));
+       double division = 10/3.4;
+       
+       System.out.println("---------------------------------------");
+       System.out.println("EJERCICIOS.");
+       System.out.println("El resultado de la suma es: "+suma);
+       System.out.println("El resultado de la resta es: "+resta);
+       System.out.println("El resultado de la division es: "+division);
+       System.out.println("El resultado de la multiplicacion es: "+multiplicacion);
+       System.out.println("El resultado del residuo es: "+residuo);
     }
     
 }
