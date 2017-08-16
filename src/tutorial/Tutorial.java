4913b49eaafe1b539bddcdfc48d33147137ab39c
Diego Alvarado <Diego Alvarado>
2017-08-15 22:27:52 -0300

    primeros pasos

---
+++ src/tutorial/Tutorial.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorial;
+
+/**
+ *
+ * @author Diego
+ */
+
+/**
+ * Tipos de datos en java
+ * short -> numeros cortos
+ * int -> numeros enteros
+ * long -> numeos enteros largos
+ * float -> numeros flotantes -> 2.12, 1.432, etc
+ * double -> numeros dobles
+ * byte ->  se maneja en bytes
+ * char -> caracteres
+ * boolean -> verdaderos o falsos
+ * String -> cadena de texto
+ *  + - * / %
+ * @author Diego
+ */
+public class Tutorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
---
+++ src/tutorial/codigofuente/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorial.codigofuente;
+
+/**
+ *
+ * @author Diego
+ */
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }
+}
